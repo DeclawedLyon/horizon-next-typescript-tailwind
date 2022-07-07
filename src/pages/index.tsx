680fb897cd48ff5291ab4fb5dd754084f8ddcffd
DeclawedLyon <declan.e.lyons@gmail.com>
2022-07-06 21:06:32 -0700

    adds styles to home screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-
+import BottomNav from '../components/navigationComponents/BottomNav'
 
 const Home: NextPage = () => {
   return (
-    <div className=' min-h-full max-h-full text-center text-cyan-500 text-xl'>
-      Hello World!
+    <div className="flex min-h-screen max-h-screen flex-col items-center justify-between">
+      <Head >
+        <title>Login</title>
+      </Head>
+      <h1 className="text-xl text-cyan-500 ">
+        Welcome! 
+      </h1>
+      <BottomNav />
     </div>
   )
 }
