f11dbdab9001667041c27dc911e71fe7a7b2313d
DeclawedLyon <declan.e.lyons@gmail.com>
2022-08-08 20:37:31 -0700

    converts login page to react component function

--- src/components/adminComponents/Login.tsx
+++ src/components/adminComponents/Login.tsx
@@ -1,47 +1,65 @@
 import React, { useEffect, useState } from 'react'
 
-export interface LoginProps {
+interface LoginProps {
 
 }
+interface LoginState {
+  userName: string,
+  password: string,
+  test: string
+}
 
-const Login = () => {
-  const [userName, setUserName] = useState('')
-  const [password, setPassword] = useState('')
-
-  const testLogin = async (username: string, password: string) => {
+class Login extends React.Component<LoginProps, LoginState> {
+  constructor(props: LoginProps) {
+    super(props)
+    this.state = {
+      userName: '',
+      password: '',
+      test: ''
+    }
+    this.testLogin = this.testLogin.bind(this)
+  }
+  async testLogin (username: string, password: string) {
     console.log(username, password)
     const loginResult = await fetch(`../../pages/api/1/test@test.com/getemployeebyemail`)
     const test = await fetch('../../pages/api/1/getemployees')
     console.log('test', test)
   }
-  const submitButton = (
-    <button
-      className='border-2 border-black rounded bg-sky-500 hover:bg-sky-700 text-xl text-white font-bold w-min px-2'
-      onClick={(e) => {
-        e.preventDefault()
-        const user = userName.trim();
-        const pass = password.trim();
-        if (user !== '' && pass !== '') {
-          const loginTest = testLogin(user, pass);
 
-        }
-      }}
-    >Submit</button>
-  )
-  useEffect(() => {
-    // console.log('test', userName, password)
-  }, [userName, password])
-  return (
-    <div className=' h-1/3 border-2 border-black p-4 relative'>
-      <h1 className='text-2xl mb-1'>Login</h1>
+//   useEffect(() => {
+//     // console.log('test', userName, password)
+//   }, [userName, password])
+
+  render() {
+    const submitButton = (
+      <button
+        className='border-2 border-black rounded bg-sky-500 hover:bg-sky-700 text-xl text-white font-bold w-min px-2'
+        onClick={(e) => {
+          e.preventDefault()
+          const user = this.state.userName.trim();
+          const pass = this.state.password.trim();
+          // if (user !== '' && pass !== '') {
+            const loginTest = this.testLogin(user, pass);
+            console.log(loginTest)
+  
+          // }
+          // this.setState({
+          //   test: 'Submit handler working'
+          // })
+        }}
+      >Submit</button>
+    )
+    return (
+      <div className=' h-1/3 border-2 border-black p-4 relative rounded-md'>
+      <h1 className='text-2xl mb-1'>Login {this.state.test}</h1>
       <form className='flex flex-col'>
         <div className='w-full flex justify-between'>
-          <label className='mr-2'>UserName</label>
-          <input type={'text'} onChange={e => setUserName(e.target.value)} className='border-2 border-black rounded mb-2 px-1'></input>
+          <label className='mr-2'>Email</label>
+          <input type={'text'} onChange={e => this.setState({userName: e.target.value})} className='border-2 border-black rounded mb-2 px-1'></input>
         </div>
         <div className='w-full flex justify-between'>
           <label className='mr-2'>Password</label>
-          <input type={"text"} onChange={e => setPassword(e.target.value)} className='border-2 border-black rounded px-1'></input>
+          <input type={"text"} onChange={e => this.setState({password: e.target.value})} className='border-2 border-black rounded px-1'></input>
         </div>
         <div className='flex w-full justify-end pt-2 -mb-2'>
           {submitButton}
@@ -49,7 +67,8 @@ const Login = () => {
         </div>
       </form>
     </div>
-  )
+    )
+  }
 }
 
 export default Login
